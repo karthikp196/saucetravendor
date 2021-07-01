908cea796b4ea3e000e8fd6294a368605b0eb4c5
karthikp196 <karthikp196@gmail.com>
2021-07-01 16:39:10 +0530

    Push Notifications

--- App.js
+++ App.js
@@ -1,52 +1,59 @@
-import * as React from 'react';
+import React , {Component} from 'react';
 import {Text , View} from 'react-native';
 import Route from './Route.js';
+import Firebase from '@react-native-firebase/app';
+import PushNotification from "react-native-push-notification";
 
-
-
-export default function App() {
-
-const [visible, setVisible] = React.useState(false);
-const onToggleSnackBar = () => setVisible(!visible);
-const onDismissSnackBar = () => setVisible(false);
-
-
-  return (
-    <Route />
-  );
+class App extends Component {
+  componentDidMount(){
+    Firebase.initializeApp(this);
+    // Push Notification Code
+    PushNotification.configure({
+      // (optional) Called when Token is generated (iOS and Android)
+      onRegister: function (token) {
+        console.log("TOKEN:", token);
+      },
+      // (required) Called when a remote is received or opened, or local notification is opened
+      onNotification: function (notification) {
+        console.log("NOTIFICATION:", notification);
+        // process the notification
+        // (required) Called when a remote is received or opened, or local notification is opened
+        notification.finish(PushNotificationIOS.FetchResult.NoData);
+      },
+      // (optional) Called when Registered Action is pressed and invokeApp is false, if true onNotification will be called (Android)
+      onAction: function (notification) {
+        console.log("ACTION:", notification.action);
+        console.log("NOTIFICATION:", notification);
+        // process the action
+      },
+      // (optional) Called when the user fails to register for remote notifications. Typically occurs when APNS is having issues, or the device is a simulator. (iOS)
+      onRegistrationError: function(err) {
+        console.error(err.message, err);
+      },
+      // IOS ONLY (optional): default: all - Permissions to register.
+      permissions: {
+        alert: true,
+        badge: true,
+        sound: true,
+      },
+      // Should the initial notification be popped automatically
+      // default: true
+      popInitialNotification: true,
+      /**
+       * (optional) default: true
+       * - Specified if permissions (ios) and token (android and ios) will requested or not,
+       * - if not, you must call PushNotificationsHandler.requestPermissions() later
+       * - if you are not using remote notification or do not have Firebase installed, use this:
+       *     requestPermissions: Platform.OS === 'ios'
+       */
+      requestPermissions: true,
+    });
+  }
+  render()
+  {
+    return(
+          <Route />
+    )
+  }
 }
-
-const styles = 
-{
-    container:{
-        flex:1,
-        backgroundColor: '#FFF',
-        paddingLeft:10,
-        paddingRight:10
-    },
-    topview:{
-      flex:2,
-      paddingTop:40
-    },
-    formview:{
-      flex:5,
-    },
-    inputs:{
-      marginTop:10
-    },
-    buttons:{
-      padding:10,
-      marginTop:10
-    },
-    footer: {
-        flex: 1,
-    },
-    textstyles:{
-      textAlign:'center',
-    },
-    headers:{
-      color:'#131DF2',
-      fontSize:30
-    }
-
-};
+export default App;
